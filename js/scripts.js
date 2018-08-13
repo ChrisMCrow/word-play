e96070f70b430f56d6e50d56ed32e2414587afaa
Chris Crow and Meria Thomas <meria@>
2018-08-13 16:47:09 -0700

    add word counter and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("button").click(function(event){
+  $("button#form1").click(function(event){
     var input = $("input#sentence").val();
     var words = input.split(" ");
     var longWords = [];
@@ -7,11 +7,25 @@ $(document).ready(function(){
       if(word.length > 3){
         longWords.push(word);
       }
-
     });
     var reverseWords = longWords.reverse();
     alert(reverseWords.join(' '));
     event.preventDefault();
   });
 
+
+  $("button#form2").click(function(event){
+    var input = $("input#object").val();
+    var words = input.split(" ");
+    var object = {}
+    words.forEach(function(word){
+      if(object[word]) {
+        object[word] += 1;
+      } else {object[word] = 1}
+    });
+    Object.keys(object).forEach(function(sortedWord) {
+      $("#output").append("<li>" + sortedWord + ": " + object[sortedWord] + "</p>")
+    });
+    event.preventDefault();
+  });
 });
